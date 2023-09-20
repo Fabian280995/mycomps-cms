64087db56ccef9fbaf9230a919b127bc062bb53b
Fabian Lessmann <lessmannwebdev@gmail.com>
2023-09-20 15:20:03 +0200

    Fixed problem with Modal-Overflow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import UsersList from "@/components/dashboard/users-list";
 import SidebarToggle from "@/components/sidebar/sidebar-toggle";
 import ClientContainer from "@/components/ui/client-container";
-import PageHeader from "@/components/ui/page-header";
 import { fetchUsers } from "@/lib/actions/user.actions";
 
 export default async function DashboardPage() {
   const users = await fetchUsers();
   return (
     <ClientContainer>
-      <PageHeader title="Dashboard" />
       <UsersList users={users} />
       <SidebarToggle />
     </ClientContainer>
--- components/ui/client-container.tsx
+++ components/ui/client-container.tsx
@@ -6,7 +6,7 @@ interface Props {
 const ClientContainer: React.FC<Props> = ({ children }) => {
   return (
     <section
-      className="w-full px-2 sm:px-4 md:px-6 lg:px-8 xl:px-12 py-4
+      className="w-full px-2 sm:px-4 md:px-6 lg:px-8 xl:px-12 py-8
       overflow-y-auto overflow-x-hidden pb-24
     "
     >
